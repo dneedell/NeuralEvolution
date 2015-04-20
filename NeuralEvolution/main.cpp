2e012da78443fd44ecfea1c271b3802fc1532897
dneedell <dneedell@bowdoin.edu>
2015-04-20 11:46:11 -0400

    Initial Commit

---
+++ NeuralEvolution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeuralEvolution
+//
+//  Created by David Robert Needell on 4/20/15.
+//  Copyright (c) 2015 David Robert Needell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
