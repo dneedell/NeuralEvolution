d05f6423b9509c1707cbe640d705d6b12dee1253
ptb415 <pbumsted@bowdoin.edu>
2015-04-30 23:55:37 -0400

    Work on reading images

--- NeuralEvolution/Problem.cpp
+++ NeuralEvolution/Problem.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "Problem.h"
+#include <iostream>
+#include <stdio.h>
+
+Problem::Problem(string fileName) {
+    // file in this code corresponds to pgm in the lab code
+    ifstream file;
+    file.open(fileName);
+    string line;
+    fgets(line, 511, file);
+    int type;
+    sscanf(line, "P%d", &type);
+    if (type != 5 && type != 2) {
+        printf("Only handles pgm files (type P5 or P2)\n");
+        file.close();
+        return;
+    }
+    /*
+     * Get dimensions of pgm
+     */
+    fgets(line, 511, file);
+    int nc;
+    int nr;
+    sscanf(line, "%d %d", &nc, &nr);
+}
