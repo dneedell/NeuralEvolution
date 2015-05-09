90aaac3666f68aee14c55cd24a2e623391efd652
rbarrett95 <rbarrett@bowdoin.edu>
2015-05-09 14:33:01 -0400

    testing loop
    loop not tested (i don’t have my scheme set up).  still needs to write
    to csv file.  alg->run() does not return anything.

--- NeuralEvolution/main.cpp
+++ NeuralEvolution/main.cpp
@@ -16,6 +16,87 @@ using namespace::std;
 
 const int epochs = 100;
 
+void testing(string trainName, string testname, int epochs, int genNum){
+    
+    double learnRate;
+    double mutProb;
+    int pop;
+    double crossProb;
+    string selection;
+    string crossover;
+    for (int lr = 0; lr < 2; lr++){
+        if(lr == 0){
+            learnRate = 0.2;
+        }
+        else{
+            learnRate = 0.3;
+        }
+                for (int s = 0; s < 3; s++){
+                    if(s == 0){
+                        selection = "TS";
+                    }
+                    else if(s == 1){
+                        selection = "RS";
+                    }
+                    else{
+                        selection = "BS";
+                    }
+                    for (int c = 0; c < 2; c++){
+                        if(c == 0){
+                            crossover = "OPC";
+                        }
+                        else{
+                            crossover = "UC";
+                        }
+                        for (int cp = 0;  cp < 2; cp++){
+                            if(cp == 0){
+                                crossProb = 0.5;
+                            }
+                            else{
+                                crossProb = 0.7;
+                            }
+                            for (int mp = 0; mp < 2; mp++){
+                                if(lr == 0){
+                                    mutProb = 0.01;
+                                }
+                                else{
+                                    mutProb = 0.1;
+                                }
+                                    for (int p = 0; p < 2; p++){
+                                        if(p == 0){
+                                            pop = 4;
+                                        }
+                                        else{
+                                            pop = 8;
+                                        }
+                                        //run each test 5 times
+                                        for(int i = 0; i < 5; i++){
+                                            NeuralEvolAlg* alg = new NeuralEvolAlg(trainName, testname, learnRate,
+                                                                                   selection, crossover, crossProb,
+                                                                                   mutProb, genNum, pop, epochs);
+                                            
+                                            //start the clock
+                                            
+                                            clock_t start = clock();
+                                            alg->run(); // alg needs to return something for this to work!
+                                            
+                                            
+                                            double time = (clock() - start)/(double)CLOCKS_PER_SEC;
+                                            //                                            saveResults(tops[topo], funcs[func], particles[numParticles],
+                                            //                                            result, time);
+                                            
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
 int main(int argc, const char * argv[]) {
    
     srand( (int) time( NULL ) );
@@ -59,6 +140,8 @@ int main(int argc, const char * argv[]) {
         int pop = stoi(argv[9]);
         
         //int epochs = stoi(argv[9]);
+        
+        testing(trainName, testname, epochs, genNum);
         
         //New instance of the Neural Evolutionary algorithm
         NeuralEvolAlg* alg = new NeuralEvolAlg(trainName, testname, learnRate,
